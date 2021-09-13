596525863c13485a866f702d76dfaf05bc5c14b3
Bogus Local PCs <bogusx_2009@mail2000.com.tw>
2021-09-13 15:56:12 +0800

    [port.py] Use argparse module.

--- port.py
+++ port.py
@@ -2,18 +2,20 @@
 import random
 import string
 import secrets
-import sys
-
-if len(sys.argv) <2:
-	count = 16
-elif len(sys.argv) >2:
-	print('Error: invalid argument count!, must one.')
-elif int(sys.argv[1]) <1:
-	print('Error: invalid number.')
-else:
-	count = int(sys.argv[1])
-
-for j in range(count):
+import argparse
+#import sys
+#if len(sys.argv) <2:
+#	count = 16
+#elif len(sys.argv) >2:
+#	print('Error: invalid argument count!, must one.')
+#elif int(sys.argv[1]) <1:
+#	print('Error: invalid number.')
+#else:
+#	count = int(sys.argv[1])
+parser = argparse.ArgumentParser(prog="port.py",description="generate random ports.")
+parser.add_arguments("count",type=int,nargs='?',default=16,help="numbers to generate.")
+arg = parser.parse_args()
+for j in range(arg.count):
 	k = random.randrange(1024,65534)
 	l = random.randrange(10240,65534)
 	m = ''.join(secrets.choice(string.digits) for q in range(4))
